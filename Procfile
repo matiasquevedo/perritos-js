0438ff3c349bf2a7eb209193decce177f3c3524f
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2020-08-06 00:59:03 -0300

    db connect

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 // Procfile
 
 release: ENV_SILENT=true node ace migration:run --force
-web: node --harmony_proxies server.js
+web: ENV_SILENT=true npm start
--- config/database.js
+++ config/database.js
@@ -19,7 +19,13 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: {
+    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+    port: Env.get('DB_PORT', ''),
+    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+  }
 
   /*
   |--------------------------------------------------------------------------

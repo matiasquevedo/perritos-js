ba3ba81c70c8690e6281d14b5d9c24623f437ff0
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2020-08-06 01:09:55 -0300

    db config

--- config/database.js
+++ config/database.js
@@ -61,15 +61,12 @@ module.exports = {
   //   }
   // },
 
-  mysql: {
-    client: 'mysql',
-    connection: {
+  connection: {
       host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
       database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-    }
   },
 
   /*
